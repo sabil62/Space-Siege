af6446010acea75112081426d4a8ee8733ab5f92
sabil62 <shresthasabil62@gmail.com>
2022-01-03 21:44:44 +0545

    key events

---
+++ prac/main.js
@@ -0,0 +1,33 @@
+let mainCharacter = document.getElementById("main-character");
+
+let mainCharacterImage = document.getElementById("main-character-image");
+
+document.addEventListener("keydown", (event) => {
+  console.log(event.code);
+  switch (event.code) {
+    case "ArrowRight":
+      mainCharacterImage.src = "../asset/images/gundam right.png";
+      break;
+
+    case "ArrowLeft":
+      mainCharacterImage.src = "../asset/images/gundam left.png";
+      break;
+
+    case "ArrowUp":
+      mainCharacterImage.src = "../asset/images/gundam up.png";
+      break;
+    case "ArrowDown":
+      mainCharacterImage.src = "../asset/images/gundam up.png";
+      break;
+    // case "KeyA":
+    case "Space":
+      mainCharacterImage.src = "../asset/images/gundam gunshoot.png";
+
+    default:
+      break;
+  }
+});
+
+document.addEventListener("keyup", (event) => {
+  mainCharacterImage.src = "../asset/images/gundam.gif";
+});

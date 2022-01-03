b9e45424a16b8bfb0c249c3092c84be17ba986d5
sabil62 <shresthasabil62@gmail.com>
2022-01-04 01:02:57 +0545

    flying enabled

--- prac/main.js
+++ prac/main.js
@@ -3,25 +3,34 @@ let mainCharacter = document.getElementById("main-character");
 let mainCharacterImage = document.getElementById("main-character-image");
 
 document.addEventListener("keydown", (event) => {
-  console.log(event.code);
+  //   console.log(event.code);
   switch (event.code) {
     case "ArrowRight":
       mainCharacterImage.src = "../asset/images/gundam right.png";
-      mainCharacter.style.left = mainCharacter.offsetLeft + 6 + "px";
+      console.log(mainCharacter.offsetLeft);
+      if (mainCharacter.offsetLeft < 1020) {
+        mainCharacter.style.left = mainCharacter.offsetLeft + 6 + "px";
+      }
 
       break;
 
     case "ArrowLeft":
       mainCharacterImage.src = "../asset/images/gundam left.png";
-      mainCharacter.style.left = mainCharacter.offsetLeft - 6 + "px";
+      if (mainCharacter.offsetLeft > 0) {
+        mainCharacter.style.left = mainCharacter.offsetLeft - 6 + "px";
+      }
       break;
 
     case "ArrowUp":
       mainCharacterImage.src = "../asset/images/gundam up.png";
-      mainCharacter.style.top = mainCharacter.offsetTop - 6 + "px";
+      if (mainCharacter.offsetTop > 0) {
+        mainCharacter.style.top = mainCharacter.offsetTop - 6 + "px";
+      }
       break;
     case "ArrowDown":
-      mainCharacter.style.top = mainCharacter.offsetTop + 6 + "px";
+      if (mainCharacter.offsetTop < 480) {
+        mainCharacter.style.top = mainCharacter.offsetTop + 6 + "px";
+      }
       break;
     // case "KeyA":
     case "Space":
@@ -33,5 +42,16 @@ document.addEventListener("keydown", (event) => {
 });
 
 document.addEventListener("keyup", (event) => {
-  mainCharacterImage.src = "../asset/images/gundam.gif";
+  switch (event.code) {
+    case "ArrowRight":
+    case "ArrowLeft":
+    case "ArrowDown":
+    case "ArrowUp":
+    case "Space":
+      mainCharacterImage.src = "../asset/images/gundam.gif";
+      break;
+
+    default:
+      break;
+  }
 });

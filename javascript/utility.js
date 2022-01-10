7427cc777fd117e498d1fe1fe06323e5b6ca538a
sabil62 <shresthasabil62@gmail.com>
2022-01-10 17:51:41 +0545

    navbar complete js utily

---
+++ javascript/utility.js
@@ -0,0 +1,20 @@
+window.onload = function () {
+  let navItems = document.getElementsByClassName("nav-items");
+  navItems[0].style.textShadow = "2px 2px 24px rgba(240,240,240,0.7)";
+  navItems[0].style.color = "rgba(210,140,101)";
+
+  function removeEffectNav() {
+    for (let i = 0; i < navItems.length; i++) {
+      navItems[i].style.textShadow = "none";
+      navItems[i].style.color = "white";
+    }
+  }
+
+  for (let i = 0; i < navItems.length; i++) {
+    navItems[i].onclick = function a() {
+      removeEffectNav();
+      navItems[i].style.textShadow = "2px 2px 24px rgba(240,240,240,0.7)";
+      navItems[i].style.color = "rgba(210,140,101)";
+    };
+  }
+};

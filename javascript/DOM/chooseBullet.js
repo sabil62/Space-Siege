7b00605fea07e4244e9dc15ddc5b11254665e783
sabil62 <shresthasabil62@gmail.com>
2022-01-11 22:13:51 +0545

    unlocking successfully achieved

--- javascript/DOM/chooseBullet.js
+++ javascript/DOM/chooseBullet.js
@@ -1,34 +1,41 @@
 let chooseBullet = document.getElementsByClassName("chooseBullet");
 
+//blureed
+chooseBullet[1].style.filter = "opacity(15%)";
+chooseBullet[2].style.filter = "opacity(10%)";
+chooseBullet[3].style.filter = "opacity(10%)";
+
 export default function chooseBullets(gameObj) {
   for (let i = 0; i < chooseBullet.length; i++) {
-    chooseBullet[i].onclick = () => {
-      let im = i + 1;
-      let coinCountDecrease = 0;
+    let im = i + 1;
+    let coinCountDecrease = 0;
 
-      if (!gameObj.bulletTypeUnlocked[i]) {
-        switch (im) {
-          case 2:
-            coinCountDecrease = 15;
-            break;
-          case 3:
-            coinCountDecrease = 40;
-            break;
-          case 4:
-            coinCountDecrease = 60;
-            break;
-        }
+    if (!gameObj.bulletTypeUnlocked[i]) {
+      switch (im) {
+        case 2:
+          coinCountDecrease = 15;
+          break;
+        case 3:
+          coinCountDecrease = 40;
+          break;
+        case 4:
+          coinCountDecrease = 60;
+          break;
       }
-
-      if (gameObj.coinCount >= coinCountDecrease) {
+    }
+    if (gameObj.coinCount >= coinCountDecrease) {
+      chooseBullet[i].style.filter = "opacity(100%)";
+      chooseBullet[i].onclick = () => {
         gameObj.decreaseCoinCount(coinCountDecrease);
         gameObj.bulletTypeUnlock(i);
         setOriginalImage();
         gameObj.player.updateBulletType(im); //do .player.updateBullet()
         gameObj.updateBulletType(im);
         chooseBullet[i].src = "./assets/UI/choose-" + im + "-hover.png";
-      }
-    };
+      };
+    } else {
+      chooseBullet[i].style.filter = "opacity(10%)";
+    }
   }
 }
 

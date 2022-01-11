67586649ba772b20abe6d29e3d127c9a57dc4710
sabil62 <shresthasabil62@gmail.com>
2022-01-11 12:13:19 +0545

    play start and ui

--- javascript/main.js
+++ javascript/main.js
@@ -6,7 +6,7 @@ let ctx = gameCanvas.getContext("2d");
 gameCanvas.width = 1745 / 1.5;
 gameCanvas.height = 928 / 1.5;
 
-let frame = 1;
+let frame = 30;
 
 let game = new Game(ctx, gameCanvas.width, gameCanvas.height, 1);
 let intervalId = setInterval(() => {
@@ -14,16 +14,20 @@ let intervalId = setInterval(() => {
   game.draw();
 }, 1000 / frame);
 
+let start = document.getElementsByClassName("start")[0];
 let pause = document.getElementsByClassName("pause")[0];
+start.style.display = "none";
 
 pause.onclick = () => {
   clearInterval(intervalId);
+  start.style.display = "block";
+  pause.style.display = "none";
 };
-
-let start = document.getElementsByClassName("start")[0];
 
 start.onclick = () => {
   intervalId = setInterval(() => {
     game.draw();
   }, 1000 / frame);
+  start.style.display = "none";
+  pause.style.display = "block";
 };
